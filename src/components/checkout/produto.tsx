d285f2f6ef2bf716a9be7f7b646e2c41514efebe
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-10-10 20:12:02 -0300

    finalizando componente de checkout

---
+++ src/components/checkout/produto.tsx
@@ -0,0 +1,39 @@
+import { Image } from "@nextui-org/image";
+import { ElementType } from "react";
+
+import Text from "../shared/text";
+
+interface ProdutosProps {
+  img: string;
+  nome: string;
+  cor: string;
+  preco: string;
+  quantidade: string;
+  icone: ElementType;
+}
+export default function Produto(props: ProdutosProps) {
+  return (
+    <div className="flex gap-7">
+      <div className="w-1/5 bg-[#ffffff] border-1 border-[#00000033] rounded-xl items-center h-24 flex justify-center">
+        <Image alt={props.nome} radius="none" src={props.img} />
+      </div>
+      <div className="w-4/5 flex flex-col justify-between">
+        <div className="flex justify-between">
+          <div>
+            <Text font="semibold" text={props.nome} />
+            <Text className="text-[#898E99]" font="semibold" text={props.cor} />
+          </div>
+          <Text font="semibold" text={props.preco} />
+        </div>
+        <div className="flex justify-between items-center">
+          <Text
+            className="text-[#898E99]"
+            font="semibold"
+            text={props.quantidade}
+          />
+          <Text className="text-[#898E99]" font="semibold" text={props.icone} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import { Image } from "@nextui-org/image";
 import { IoClose } from "react-icons/io5";
 
 import Header from "@/components/header";
 import DefaultLayout from "@/layouts/default";
-import Text from "@/components/shared/text";
+import Produto from "@/components/checkout/produto";
 
 export default function IndexPage() {
   return (
@@ -14,33 +13,32 @@ export default function IndexPage() {
       >
         <Header />
         <div className="z-10 w-full h-full flex flex-col rounded-2xl bg-[#FFFFFF] overflow-hidden">
-          <div className="flex flex-col bg-[#E6E7EB] gap-5 p-5">
-            <div className="flex gap-4">
-              <div className="w-3/12 bg-[#ffffff] border-1 border-[#00000033] rounded-xl p-3 flex justify-center">
-                <Image alt="gold" radius="none" src="/images/Gold.png" />
-              </div>
-              <div className="w-9/12 flex flex-col justify-between">
-                <div className="flex justify-between">
-                  <div>
-                    <Text font="semibold" text="iPhone 12 Pro" />
-                    <Text
-                      className="text-[#898E99]"
-                      font="semibold"
-                      text="Golden"
-                    />
-                  </div>
-                  <Text font="semibold" text="$999.00" />
-                </div>
-                <div className="flex justify-between items-center">
-                  <Text className="text-[#898E99]" font="semibold" text="x 1" />
-                  <Text
-                    className="text-[#898E99]"
-                    font="semibold"
-                    text={IoClose}
-                  />
-                </div>
-              </div>
-            </div>
+          <div className="flex flex-col bg-[#E6E7EB] gap-5 py-5 px-7">
+            <Produto
+              cor="Golden"
+              icone={IoClose}
+              img="/images/Gold.png"
+              nome="iPhone 12 Pro"
+              preco="$999.00"
+              quantidade="x 1"
+            />
+            <Produto
+              cor="Golden"
+              icone={IoClose}
+              img="/images/iMacFront1.png"
+              nome="iPhone 12 Pro"
+              preco="$999.00"
+              quantidade="x 1"
+            />
+            <Produto
+              cor="Golden"
+              icone={IoClose}
+              img="/images/Gold.png"
+              nome="iPhone 12 Pro"
+              preco="$999.00"
+              quantidade="x 1"
+            />
+
             <hr className="border-[#0000001a]" />
           </div>
         </div>
